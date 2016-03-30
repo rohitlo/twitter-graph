e421fb8bdf2ad11728a91114916b03a560394e92
Rahul Gopinath <rahul@gopinath.org>
2016-03-29 21:55:00 -0700

    cleanit.rb add version check

--- bin/cleanit.rb
+++ bin/cleanit.rb
@@ -36,6 +36,7 @@ def process(my_hash)
     # are guaranteed to be unique within a single process.
     # This is OK for us because we are computing _online_, and are
     # not worried about persistance of graph nodes.
+    # DEBUG: nodes = hset.to_a.map(&:to_s).sort
     nodes = hset.to_a.map(&:hash).sort
     return {:ctime => ctime, :nodes => nodes}
   rescue
@@ -45,6 +46,14 @@ def process(my_hash)
   end
 end
 
+case RUBY_VERSION
+when /^1./
+  puts "Require at least 2.2"
+  exit 1
+when /^2.[01]/
+  puts "Require at least 2.2"
+  exit 1
+end
 
 ARGF.each do |l|
   v = process(JSON.parse(l))
@@ -52,6 +61,7 @@ ARGF.each do |l|
 
   # Generate edges with creating time attached.
   v[:nodes].combination(2).each do |n|
+    # DEBUG p v
     print v[:ctime],',',n.join(','), "\n"
   end
   STDOUT.flush
