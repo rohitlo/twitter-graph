d3e693f99a618a0fba73e49f1c0d2758e657b7d4
Rahul Gopinath <rahul@gopinath.org>
2016-03-30 23:02:22 -0700

    cosmetic changes

--- bin/tweetview.py
+++ bin/tweetview.py
@@ -1,84 +1,127 @@
 #!/usr/bin/env python3
+"""
+This module computes the rolling average indegree of a twitter
+tweet hashtag graph.
+"""
 import sys
 import json
 import time
-from heapdict import heapdict
 import itertools
+from heapdict import heapdict
 
 WINDOW = 60
+TIME_FMT = "%a %b %d %H:%M:%S +0000 %Y"
 
-class Processor:
-    def __init__(self):
-        self.latest = 0
+
+class TweetProcessor:
+    """
+    Process the tweet, and keeps track of the time.
+    """
+    def __init__(self, curtime):
+        self.latest = curtime
         self.edges = {}
         self.queue = heapdict()
 
-
-    def is_valid(self, ctime):
+    def in_window(self, ctime):
+        """
+        Is the passed in creation time within the window?
+        """
         return False if (self.latest - ctime) >= WINDOW else True
 
-
     def add_edge(self, ctime, edge):
+        """
+        Add or update the given edge with the given time to
+        our database of edges.
+        """
         edge_key = ' '.join(map(str, edge))
         old_ctime = self.edges.get(edge_key, None)
         if (not old_ctime) or (ctime > old_ctime):
             self.queue[edge_key] = ctime
             self.edges[edge_key] = ctime
 
-
     def process(self, ctime, nodes):
-        if not self.is_valid(ctime): return
+        """
+        Process the given set of hashtags for the given time.
+        """
+        if not self.in_window(ctime):
+            return
 
         if ctime > self.latest:
             self.latest = ctime
 
-        self.gc()
+        self.dogc()
 
         for edge in itertools.combinations(nodes, 2):
             self.add_edge(ctime, edge)
 
     def gc_complete(self):
-        if len(self.queue) == 0: return True
-        edge, ctime = self.queue.peekitem()
-        return self.is_valid(ctime)
+        """
+        Check if the GC can be stopeed (Does any edge remain
+        that can be removed?)
+        """
+        if len(self.queue) == 0:
+            return True
+        _, ctime = self.queue.peekitem()
+        return self.in_window(ctime)
 
-    def gc(self):
+    def dogc(self):
+        """
+        Perform garbage collection.
+        """
         while not self.gc_complete():
-            min_edge, ctime = self.queue.popitem()
+            min_edge, _ = self.queue.popitem()
             del self.edges[min_edge]
 
-    def avg(self):
-        if not self.edges: return 0
+    def average(self):
+        """
+        Compute the average degree of a vertex.
+        """
+        if not self.edges:
+            return 0
         nodes = set()
         for l_r in self.edges.keys():
             nodes.update(l_r.split(' '))
         return (2.0 * len(self.edges))/len(nodes)
 
-def process(my_hash):
+
+def check_json(my_hash):
+    """
+    Initial processing of the json line.
+    """
     created_at = my_hash.get('created_at', None)
-    if not created_at: return None
-    ctime = int(time.mktime(time.strptime(created_at, "%a %b %d %H:%M:%S +0000 %Y")))
+    if not created_at:
+        return None
+    ctime = int(time.mktime(time.strptime(created_at, TIME_FMT)))
 
     entities = my_hash.get('entities', None)
-    if not entities: return None
+    if not entities:
+        return None
 
     htags = entities.get('hashtags', None)
-    if not htags: return None
+    if not htags:
+        return None
 
-    hset = set([hm['text'] for hm in  htags])
-    if len(hset) < 2: return None
+    hset = set(hm['text'] for hm in htags)
+    if len(hset) < 2:
+        return None
 
-    nodes = sorted(hash(a)  for a in hset)
-    return {'ctime':ctime, 'nodes':nodes}
-
-current = Processor()
-for line in sys.stdin:
-    v = process(json.loads(line))
-    if not v:
-        print(current.avg())
-        continue
-
-    current.process(v['ctime'], v['nodes'])
-    print(current.avg())
+    nodes = sorted(hash(a) for a in hset)
+    return {'ctime': ctime, 'nodes': nodes}
 
 
+def main():
+    """
+    The entry point.
+    """
+    tweeter = TweetProcessor(0)
+    for line in sys.stdin:
+        jhash = check_json(json.loads(line))
+        if not jhash:
+            print(tweeter.average())
+            continue
+
+        tweeter.process(jhash['ctime'], jhash['nodes'])
+        print(tweeter.average())
+
+if __name__ == "__main__":
+    main()
