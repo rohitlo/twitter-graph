93a6d3e552f3c0a8be8632afcffd99f99d054c70
Rahul Gopinath <rahul@gopinath.org>
2016-03-31 13:57:27 -0700

    Change the run

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 # example of the run script for running the word count
 
 # I'll execute my programs, with the input directory tweet_input and output the files in the directory tweet_output
-python ./src/average_degree.py ./tweet_input/tweets.txt ./tweet_output/output.txt
+python3.5 ./src/average_degree.py < ./tweet_input/tweets.txt > ./tweet_output/output.txt
 
 
 
