6e2bc30c8a6e787b504ff306e840bcf70bf96e1c
Rahul Gopinath <rahul@gopinath.org>
2016-04-01 00:46:49 -0700

    generator

---
+++ src/gentweet.sh
@@ -0,0 +1,13 @@
+
+input="insight_testsuite/tests/$1/tweet_input"
+output="insight_testsuite/tests/$1/tweet_output"
+mkdir -p $input $output
+shift
+t=$1
+shift
+echo $input $output
+echo time = $t
+echo nodes = $@
+./src/gen-tweet.py $t $@ >> $input/tweets.txt
+cat $input/tweets.txt | ./src/average_degree.py > $output/output.txt
+cat $output/output.txt
